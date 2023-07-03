29f29fd023251986e7f6bdd7ad4b13bc6a6e2b35
mohdimtiyaz002 <76655817+mohdimtiyaz002@users.noreply.github.com>
2023-07-03 21:22:18 +0530

    Update test_my_website.py

--- test_my_website.py
+++ test_my_website.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_my_website():
     # Send a GET request to the website
-    response = requests.get("http://54.241.214.169:5000/")
+    response = requests.get("http://54.219.131.162:5000/")
 
     # Assert that the response status code is 200 (OK)
     assert response.status_code == 200
